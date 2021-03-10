bef535d11565f2f96dd30fd84549abb92c9996ab
jimmy <934757965@qq.com>
2021-03-10 17:27:54 +0800

    add animation character

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+    private Animator ani;
+	// Use this for initialization
+	void Start () {
+        ani = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float v = Input.GetAxisRaw("Vertical");
+        ani.SetInteger("Vertical", (int)v);
+        transform.Translate(Vector3.forward * v * Time.deltaTime * 3);
+	}
+}
---
+++ Assets/Scripts/musicOn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class musicOn : MonoBehaviour {
+
+    private Toggle toogle;
+    public GameObject toogleOn;
+    public GameObject toogleOff;
+	// Use this for initialization
+	void Start () {
+        toogle = GetComponent<Toggle>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnToggleValueChange(bool isOn)
+    {
+        toogleOn.SetActive(isOn);
+        toogleOff.SetActive(!isOn);
+    }
+}
