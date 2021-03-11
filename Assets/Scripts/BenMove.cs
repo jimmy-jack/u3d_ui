57bdbda1116f12e39fa90eb4e584800bee37f37f
jimmy <934757965@qq.com>
2021-03-11 17:31:39 +0800

    humanoid animation & official avatar motion

---
+++ Assets/Scripts/BenMove.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BenMove : MonoBehaviour {
+
+    private Animator anim;
+    private int speedID = Animator.StringToHash("Speed");
+    private int isSpeedUpID = Animator.StringToHash("IsSpeedUp");
+    private int turnID = Animator.StringToHash("Horizontal");
+
+
+    private int speedRotateID = Animator.StringToHash("SpeedRotate");
+    private int speedZID = Animator.StringToHash("SpeedZ");
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //anim.SetFloat("Speed", Input.GetAxis("Vertical"));
+        //anim.SetFloat(speedID, Input.GetAxis("Vertical"));
+        //anim.SetFloat(turnID, Input.GetAxis("Horizontal"));
+        //if(Input.GetKeyDown(KeyCode.LeftShift))
+        //{
+        //    anim.SetBool(isSpeedUpID, true);
+        //}
+        //if(Input.GetKeyUp(KeyCode.LeftShift))
+        //{
+        //    anim.SetBool(isSpeedUpID, false);
+        //
+
+        //anim.SetFloat(speedID, Input.GetAxis("Vertical") * 3.551f);
+
+
+        anim.SetFloat(speedZID, Input.GetAxis("Vertical") * 3.551f);
+        anim.SetFloat(speedRotateID, Input.GetAxis("Horizontal") * 2.2f);
+	}
+}
---
+++ Assets/Scripts/CamMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMove : MonoBehaviour {
+
+    private Transform player;
+    private Vector3 offset;
+    private int smoothness = 3;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        offset = transform.position - player.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        Vector3 targetPos = player.position + player.TransformDirection(offset);
+
+        //跟随
+        transform.position = Vector3.Lerp(transform.position, targetPos, Time.deltaTime * smoothness);
+        transform.LookAt(player);
+	}
+}
